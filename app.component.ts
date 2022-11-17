5651c969be57e55bff82652c6b4177ff35594222
Eduardo M <110671336+EMSMyesmkt@users.noreply.github.com>
2022-11-17 19:23:33 +0000

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'To Do List';
+  list: any[] = [];
+  
+    addTask(item: string)
+    {
+      console.log(item);
+      this.list.push({id:this.list.length, name:item})
+      console.log(this.list);
+    }
+
+    removeTask(id: number)
+    {
+      console.log(id)
+      this.list = this.list.filter(item => item.id! == id);
+    }
+}
